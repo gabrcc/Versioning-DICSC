1d23bb3e3121b0bba816127e9c4d091bda6c2862
gabrcc <cuellar.caero.gabriela@gmail.com>
2025-03-10 17:31:12 -0400

    Funcionalidades de suma y resta agregadas

---
+++ calculadora.py
@@ -0,0 +1,38 @@
+import sys 
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def main():
+    print("\nCalculadora basica - Version 1.0")
+    while True:
+        
+        print("\n1. Suma")
+        print("2. Resta")
+        print("3. Exit")
+
+        opcion = input("\nIngrese el numero de la operacion a realizar: ")
+        if opcion == "3":
+            print("EXIT")
+            sys.exit()
+
+        if opcion not in ["1", "2"]:
+            print("Opcion invalida")
+            continue
+        
+        try:
+            n1 = float(input("\nIngrese el primer numero: "))
+            n2 = float(input("Ingrese el segundo numero: "))
+        except ValueError:
+            print("\nEntrada invalida")
+            continue
+        if opcion == "1":
+            print(f"\n******Resultado: {suma(n1, n2)}******")
+        elif opcion == "2":
+            print(f"\n******Resultado: {resta(n1, n2)}******")   
+
+if __name__ == "__main__":
+    main()            
